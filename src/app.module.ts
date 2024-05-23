bdbd488508eb1d12d42a1e61253b6e51b91774da
Kruuux <kruczekkonrad.kk@gmail.com>
2024-05-23 11:56:36 +0200

    generate crud template from CLI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+import { LibrariesModule } from './libraries/libraries.module';
 
 @Module({
-  imports: [],
+  imports: [BooksModule, LibrariesModule],
   controllers: [AppController],
   providers: [AppService],
 })
