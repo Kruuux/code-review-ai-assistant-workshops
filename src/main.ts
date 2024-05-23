5b3fc2ccdd3802176924e33340295b15dd3fe946
Kruuux <kruczekkonrad.kk@gmail.com>
2024-05-23 13:25:22 +0200

    add api key

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const API_KEY =
+    'd87e842811e3c2086d1f6cfe6eede0c44b3a62d78e3e7c17050ee77d0f83f5bc';
+
+  console.log(API_KEY);
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
