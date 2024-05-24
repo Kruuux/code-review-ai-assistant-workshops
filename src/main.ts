270544543c483e26ae8f6c9c71f286ee1a6be3cf
Kruuux <kruczekkonrad.kk@gmail.com>
2024-05-24 14:22:52 +0200

    add vulnerabilities

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const API_KEY = 'AJDSFKLAJSDBFKLASNDFKLANKFDNASLKF';
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
