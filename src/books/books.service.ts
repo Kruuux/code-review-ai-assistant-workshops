364b0238f7c69cd29b32b5e491d0e588f00f3db8
Kruuux <kruczekkonrad.kk@gmail.com>
2024-05-24 12:13:40 +0200

    remove vulnerabilities

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -10,40 +10,4 @@ export class BooksService {
     @Inject('BOOK_REPOSITORY')
     private bookRepository: Repository<Book>,
   ) {}
-
-  async create(createBookDto: CreateBookDto) {
-    const book = await this.bookRepository.create(createBookDto);
-    return this.bookRepository.save(book);
-  }
-
-  findByName(name: string) {
-    return this.bookRepository.query(
-      `SELECT * FROM book WHERE name = '${name}'`,
-    );
-  }
-
-  findAll() {
-    return this.bookRepository.find();
-  }
-
-  findOne(id: number) {
-    return this.bookRepository.findOne({ where: { id } });
-  }
-
-  async update(id: number, updateBookDto: UpdateBookDto) {
-    const book = await this.bookRepository.findOne({ where: { id } });
-    if (!book) throw new NotFoundException(`Book #${id} not found`);
-
-    book.name = updateBookDto.name;
-    book.description = updateBookDto.description;
-
-    return this.bookRepository.update({ id }, book);
-  }
-
-  async remove(id: number) {
-    const book = await this.bookRepository.findOne({ where: { id } });
-    if (!book) throw new NotFoundException(`Book #${id} not found`);
-
-    return this.bookRepository.remove(book);
-  }
 }
