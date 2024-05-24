ed96c21462fd95466367f136ae311c79dbe79fa5
Kruuux <kruczekkonrad.kk@gmail.com>
2024-05-24 14:30:38 +0200

    add github scanner

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -16,6 +16,12 @@ export class BooksService {
     return this.bookRepository.save(book);
   }
 
+  findByName(name: string) {
+    return this.bookRepository.query(
+      `SELECT * FROM book WHERE name = '${name}'`,
+    );
+  }
+
   findAll() {
     return this.bookRepository.find();
   }
